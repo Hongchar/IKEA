22417c6b5c8611b12751d82680165d13b51317b3
Oh-hyejun <qwegw0303@naver.com>
2024-06-21 12:13:38 +0900

    Add files via upload

---
+++ src/tool/HomeButton.java
@@ -0,0 +1,19 @@
+package tool;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
+public class HomeButton extends JButton {
+	// 홈버튼
+	public HomeButton() {
+		super(new ImageIcon("imgs/home.png"));
+		
+		setBounds(340, 7, 36, 36);
+		setBackground(new Color(16, 118, 200));
+		setOpaque(false);
+		setBorder(null);
+	}
+	
+}
